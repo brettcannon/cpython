91be9fe21e475602debbf7bda7e7b839b56112ec
Brett Cannon <brett@python.org>
2026-04-12 14:24:48 -0700

    Add `project_root` to venv.create()

--- Lib/venv/__init__.py
+++ Lib/venv/__init__.py
@@ -624,13 +624,13 @@ class EnvBuilder:
 
 def create(env_dir=DEFAULT_NAME, system_site_packages=False, clear=False,
            symlinks=False, with_pip=False, prompt=None, upgrade_deps=False,
-           *, scm_ignore_files=frozenset()):
+           *, scm_ignore_files=frozenset(), project_root=None):
     """Create a virtual environment in a directory."""
     builder = EnvBuilder(system_site_packages=system_site_packages,
                          clear=clear, symlinks=symlinks, with_pip=with_pip,
                          prompt=prompt, upgrade_deps=upgrade_deps,
                          scm_ignore_files=scm_ignore_files)
-    builder.create(env_dir)
+    builder.create(env_dir, project_root=project_root)
 
 
 def executable(dir, *, traverse=False):
